a61dc4e13c85ab7bd5ebc9059f35997a53fda985
gc <30398469+gach@users.noreply.github.com>
2018-05-23 16:52:15 +1000

    add NoteListEmpyItem and testing

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { createContainer } from "meteor/react-meteor-data";
 
-import { Notes } from '../api/notes';
-import NoteListHeader from './NoteListHeader';
-import NoteListItem from './NoteListItem';
+import { Notes } from "../api/notes";
+import NoteListHeader from "./NoteListHeader";
+import NoteListItem from "./NoteListItem";
+import NoteListEmptyItem from "./NoteListEmptyItem";
 
-export const NoteList = (props) => {
-  return (
-    <div>
-      <NoteListHeader/>
-      {props.notes.map((note) => {
-        return <NoteListItem key={note._id} note={note}/>;
-      })}
-      NoteList { props.notes.length }
-    </div>
-  );
+export const NoteList = props => {
+    return (
+        <div>
+            <NoteListHeader />
+            {props.notes.map(note => {
+                return <NoteListItem key={note._id} note={note} />;
+            })}
+            {props.notes.length === 0 && <NoteListEmptyItem />}
+            NoteList {props.notes.length}
+        </div>
+    );
 };
 
 NoteList.propTypes = {
-  notes: React.PropTypes.array.isRequired
+    notes: React.PropTypes.array.isRequired
 };
 
 export default createContainer(() => {
-  Meteor.subscribe('notes');
+    Meteor.subscribe("notes");
 
-  return {
-    notes: Notes.find().fetch()
-  };
+    return {
+        notes: Notes.find().fetch()
+    };
 }, NoteList);
