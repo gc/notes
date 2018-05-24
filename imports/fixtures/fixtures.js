483a4f02ea9854bad3107a5c2a43e6e3b9b11a84
gc <30398469+gach@users.noreply.github.com>
2018-05-24 17:05:43 +1000

    .

---
+++ imports/fixtures/fixtures.js
@@ -0,0 +1,16 @@
+export const notes = [
+  {
+    _id: "noteId1",
+    title: "Test title",
+    body: "",
+    updatedAt: 1526692648486,
+    userId: "userId1"
+  },
+  {
+    _id: "noteId2",
+    title: "",
+    body: "asdf",
+    updatedAt: 1486137505429,
+    userId: "userId2"
+  }
+];
--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,40 +1,49 @@
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Meteor } from "meteor/meteor";
+import React from "react";
+import { Router, Route, browserHistory } from "react-router";
 
-import Signup from '../ui/Signup';
-import Dashboard from '../ui/Dashboard';
-import NotFound from '../ui/NotFound';
-import Login from '../ui/Login';
+import Signup from "../ui/Signup";
+import Dashboard from "../ui/Dashboard";
+import NotFound from "../ui/NotFound";
+import Login from "../ui/Login";
 
-const unauthenticatedPages = ['/', '/signup'];
-const authenticatedPages = ['/dashboard'];
+const unauthenticatedPages = ["/", "/signup"];
+const authenticatedPages = ["/dashboard"];
 const onEnterPublicPage = () => {
   if (Meteor.userId()) {
-    browserHistory.replace('/dashboard');
+    browserHistory.replace("/dashboard");
   }
 };
 const onEnterPrivatePage = () => {
   if (!Meteor.userId()) {
-    browserHistory.replace('/');
+    browserHistory.replace("/");
   }
 };
-export const onAuthChange = (isAuthenticated) => {
+export const onAuthChange = isAuthenticated => {
   const pathname = browserHistory.getCurrentLocation().pathname;
   const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
   const isAuthenticatedPage = authenticatedPages.includes(pathname);
 
   if (isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.replace('/dashboard');
+    browserHistory.replace("/dashboard");
   } else if (isAuthenticatedPage && !isAuthenticated) {
-    browserHistory.replace('/');
+    browserHistory.replace("/");
   }
 };
 export const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="*" component={NotFound}/>
+    <Route path="/" component={Login} onEnter={onEnterPublicPage} />
+    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage} />
+    <Route
+      path="/dashboard"
+      component={Dashboard}
+      onEnter={onEnterPrivatePage}
+    />
+    <Route
+      path="/dashboard/:id"
+      component={Dashboard}
+      onEnter={onEnterPrivatePage}
+    />
+    <Route path="*" component={NotFound} />
   </Router>
 );
